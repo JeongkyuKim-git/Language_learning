0d142e4a9e1b1e5bd08dbc005135b01c2b678b16
JeongkyuKim-git <jkkim@mme.dongguk.edu>
2021-05-20 18:31:41 +0900

    module
    python module

---
+++ Python_3/Basic/Module/Python_module/Calculator.py
@@ -0,0 +1,15 @@
+# 더하기
+def add(x,y):
+    return x+y
+
+# 빼기
+def subtract(x,y):
+    return x-y
+
+# 곱하기
+def multiply(x,y):
+    return x*y
+
+# 나누기
+def divide(x,y):
+    return x/y
---
+++ Python_3/Basic/Module/Python_module/Example.py
@@ -0,0 +1,11 @@
+# as는 별칭
+import Calculator as calc
+
+# from calculator import add, multiply
+"""
+이파일 내에서 함수를 불러오는 방법
+print(add(1, 2))
+"""
+# from calculator import *
+
+print(calc.add(1, 2))
