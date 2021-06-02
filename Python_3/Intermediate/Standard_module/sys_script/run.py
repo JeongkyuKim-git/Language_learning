e4ec765a33328b475167ad332a25152e1b1bb5c5
JeongkyuKim-git <jkkim@mme.dongguk.edu>
2021-06-02 23:21:49 +0900

    asd

---
+++ Python_3/Intermediate/Standard_module/sys_script/run.py
@@ -0,0 +1,11 @@
+print('run 파일 이름: {}'.format(__name__))
+#
+# import area
+#
+# x = float(input('원의 지름을 입력해 주세요:'))
+# print('반지름이 {}인 원의 면적은 {}입니다.\n'.format(x, area.circle(x)))
+#
+# y = float(input('정사각형의 변의 길이를 입력해 주세요:'))
+# print('변의 길이가 {}인 정사각형의 면적은 {}입니다.\n'.format(y.area.square(y)))
+
+print("run 파일 실행됨")
